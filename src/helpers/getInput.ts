99ac4fb138dac185c583e8ce1586ffe9589a4e91
Crawron <azertyaaron@gmail.com>
2019-12-03 19:38:38 -0500

    updated getInput

--- src/helpers/getInput.ts
+++ src/helpers/getInput.ts
@@ -4,5 +4,5 @@ import { promisify } from "util"
 const readFileAsync = promisify(readFile)
 
 export async function getInput(url: string) {
-  return (await readFileAsync(url)).toString()
+  return (await readFileAsync(url + "/input")).toString()
 }
