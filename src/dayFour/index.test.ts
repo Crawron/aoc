546ea29745114998d1114446b2111b6abb0ade9d
Crawron <azertyaaron@gmail.com>
2019-12-04 01:41:15 -0500

    day 4

---
+++ src/dayFour/index.test.ts
@@ -0,0 +1,39 @@
+import { checkDecreasing, checkRepeating, checkStrictDoubles } from "."
+
+it.each([
+  ["123456", false],
+  ["221458", true],
+  ["222222", false],
+  ["654123", true],
+  ["123454", true],
+])("detects decreasing numbers", (a, b) => {
+  expect(checkDecreasing(a as string)).toBe(b)
+})
+
+it.each<[string, boolean]>([
+  ["123456", false],
+  ["112456", true],
+  ["121212", false],
+  ["155133", true],
+  ["111111", true],
+  ["222784", true],
+  ["456123", false],
+  ["784134", false],
+  ["789441", true],
+])("detects repeating numbers", (a, b) => {
+  expect(checkRepeating(a as string)).toBe(b)
+})
+
+it.each<[string, boolean]>([
+  ["123456", false],
+  ["112456", true],
+  ["121222", false],
+  ["155133", true],
+  ["111122", true],
+  ["222784", false],
+  ["456123", false],
+  ["784134", false],
+  ["789441", true],
+])("detects strict doubles", (a, b) => {
+  expect(checkStrictDoubles(a)).toBe(b)
+})
---
+++ src/dayFour/index.ts
@@ -0,0 +1,49 @@
+import { getInput } from "../helpers/getInput"
+
+export function checkDecreasing(n: string): boolean {
+  const digits = n.split("").map(Number)
+
+  for (let i = 1; i < digits.length; i++) {
+    if (digits[i - 1] > digits[i]) return true
+  }
+
+  return false
+}
+
+export function checkRepeating(n: string): boolean {
+  return /(.)\1/.test(n)
+}
+
+export function checkStrictDoubles(n: string): boolean {
+  const regex = /(\d)\1+/g
+  const matches = n.match(regex)
+
+  if (!matches) return false
+
+  const doubleMatches = matches.filter(match => match.length === 2)
+
+  return doubleMatches.length > 0
+}
+
+function partOne(input: string) {
+  const split = input.split("-").map(Number)
+  const from = split[0]
+  const to = split[1]
+
+  let counter = 0
+
+  for (let i = from; i <= to; i++) {
+    const code = i.toString()
+
+    if (!checkStrictDoubles(code)) continue
+    if (checkDecreasing(code)) continue
+
+    counter++
+  }
+
+  return counter
+}
+
+;(async function() {
+  console.log(partOne(await getInput(__dirname)))
+})()
