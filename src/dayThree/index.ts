c02ff5a1ab5b8a24f8d5fb8a4f8021b421a7a976
Crawron <azertyaaron@gmail.com>
2019-12-03 21:10:24 -0500

    day 3 part 2 working

--- src/dayThree/index.ts
+++ src/dayThree/index.ts
@@ -3,6 +3,15 @@ import { getInput } from "../helpers/getInput"
 
 export class Vec2 {
   constructor(public x = 0, public y = 0) {}
+
+  get manhattanMag(): number {
+    return Math.abs(this.x) + Math.abs(this.y)
+  }
+
+  distTo(vec: Vec2) {
+    const { abs } = Math
+    return new Vec2(vec.x - this.x, vec.y - this.y).manhattanMag
+  }
 }
 
 export class Line {
@@ -16,19 +25,44 @@ export class Line {
     this.start = start
     this.end = end
   }
+
+  get length(): number {
+    return this.start.distTo(this.end)
+  }
 }
 
-export type Wire = Line[]
+class WireSegment {
+  constructor(public line: Line, public travel: number) {}
+
+  get start() {
+    return this.line.start
+  }
+  get end() {
+    return this.line.end
+  }
+
+  get length() {
+    return this.line.length
+  }
+}
+
+export type Wire = WireSegment[]
 
 export type Direction = "U" | "D" | "L" | "R"
-export type Step = {
-  direction: Direction
-  distance: number
+
+export class Intersection {
+  constructor(public pos: Vec2, public travel: number) {}
 }
 
-function mapWire(steps: Step[]): Line[] {
-  const wireLines: Line[] = []
+export type Step = {
+  direction: Direction
+  travel: number
+}
+
+function mapWire(steps: Step[]): Wire {
+  const wire: Wire = []
   let pos: Vec2 = new Vec2()
+  let distanceTraveled = 0
 
   for (const step of steps) {
     const move: { [key in Direction]: (dist: number) => Vec2 } = {
@@ -38,12 +72,17 @@ function mapWire(steps: Step[]): Line[] {
       L: dist => new Vec2(pos.x - dist, pos.y),
     }
 
-    const endPos = move[step.direction](step.distance)
-    wireLines.push(new Line(pos, endPos))
+    const endPos = move[step.direction](step.travel)
+    const line = new Line(pos, endPos)
+    const segment = new WireSegment(line, distanceTraveled)
+
+    wire.push(segment)
+
     pos = endPos
+    distanceTraveled += Math.abs(segment.length)
   }
 
-  return wireLines
+  return wire
 }
 
 export function checkVert(line: Line): boolean {
@@ -56,7 +95,10 @@ export function checkInRange(point: number, start: number, end: number) {
   return sortedStart < point && point < sortedEnd
 }
 
-export function getIntersection(a: Line, b: Line): Vec2 | undefined {
+export function getIntersection(
+  a: WireSegment,
+  b: WireSegment
+): Intersection | undefined {
   // if lines are parallel, they wont intersect
   if (checkVert(a) === checkVert(b)) return undefined
 
@@ -67,14 +109,22 @@ export function getIntersection(a: Line, b: Line): Vec2 | undefined {
       checkInRange(b.start.y, a.start.y, a.end.y)
 
     if (!theyIntersect) return undefined
-    return new Vec2(a.start.x, b.start.y)
+
+    const pos = new Vec2(a.start.x, b.start.y)
+    const travelA = Math.abs(a.travel) + Math.abs(a.start.distTo(pos))
+    const travelB = Math.abs(b.travel) + Math.abs(b.start.distTo(pos))
+    return new Intersection(pos, travelA + travelB)
   } else {
     let theyIntersect =
       checkInRange(b.start.x, a.start.x, a.end.x) &&
       checkInRange(a.start.y, b.start.y, b.end.y)
 
     if (!theyIntersect) return undefined
-    return new Vec2(b.start.x, a.start.y)
+
+    const pos = new Vec2(b.start.x, a.start.y)
+    const travelA = Math.abs(a.travel) + Math.abs(a.start.distTo(pos))
+    const travelB = Math.abs(b.travel) + Math.abs(b.start.distTo(pos))
+    return new Intersection(pos, travelA + travelB)
   }
 }
 
@@ -82,17 +132,13 @@ function checkArraysEqual(a1: Vec2, a2: Vec2) {
   return JSON.stringify(a1) == JSON.stringify(a2)
 }
 
-function getManhattanMagnitude(point: Vec2) {
-  return Math.abs(point.x) + Math.abs(point.y)
-}
-
 function getWiresIntersections(a: Wire, b: Wire) {
-  const intersections: Vec2[] = []
+  const intersections: Intersection[] = []
 
   for (const lineA of a) {
     for (const lineB of b) {
       const intersection = getIntersection(lineA, lineB)
-      if (intersection && !checkArraysEqual(intersection, new Vec2()))
+      if (intersection && new Vec2().distTo(intersection.pos) > 0)
         intersections.push(intersection)
     }
   }
@@ -100,6 +146,7 @@ function getWiresIntersections(a: Wire, b: Wire) {
   return intersections
 }
 
+// Part 1
 async function doTheThing() {
   const input = (await getInput(__dirname))
     .split("\n")
@@ -113,7 +160,7 @@ async function doTheThing() {
       const split = splitString(step, 1)
       const parsedStep: Step = {
         direction: split[0] as Direction,
-        distance: Number(split[1]),
+        travel: Number(split[1]),
       }
       steps.push(parsedStep)
     }
@@ -121,11 +168,14 @@ async function doTheThing() {
     wires.push(mapWire(steps))
   }
 
-  const intersections = getWiresIntersections(wires[0], wires[1])
+  const intersections = getWiresIntersections(wires[0], wires[1]).sort(
+    (a, b) => a.travel - b.travel
+  )
   const distances = intersections
-    .map(getManhattanMagnitude)
+    .map(i => i.pos.manhattanMag)
     .sort((a, b) => a - b)
 
+  console.log(intersections)
   console.log(`Solution:\nThe closest intersection: ${distances[0]}`)
 }
 
